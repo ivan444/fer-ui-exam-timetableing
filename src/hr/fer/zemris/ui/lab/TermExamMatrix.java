7369ec22749391181087bcbca888167afe647e22
ivan.kristo <ivan.kristo@f489f2c6-395e-11de-a9fe-ad1e22fc3a8a>
2009-07-03 17:41:22 +0000

    ukljucena struktura umjesto mape al jos ne radi ok

--- src/hr/fer/zemris/ui/lab/TermExamMatrix.java
+++ src/hr/fer/zemris/ui/lab/TermExamMatrix.java
@@ -9,9 +9,11 @@ import hr.fer.zemris.ui.lab.generator.beans.TermBean;
 // TODO: Pretraživanje je izvedeno petljama a ne hashevima. Ovo možda i nije takav problem jer se radi o malom broju termina, a hash kolekcije rade ekstra posao koji nam ne treba. 
 public class TermExamMatrix {
 	private TermExams[] termExams;
+	private int cachedIndex;
 	
 	public TermExamMatrix(TermBean[] allTerms, int examsNum) {
 		this.termExams = new TermExams[allTerms.length];
+		cachedIndex = 0;
 		
 		for (int i = 0; i < allTerms.length; i++) {
 			this.termExams[i] = new TermExams(allTerms[i], examsNum);
@@ -19,16 +21,27 @@ public class TermExamMatrix {
 	}
 	
 	public void addTermExam(TermBean term, ExamBean exam) {
+		if (termExams[cachedIndex].wrapsTerm(term)) {
+			termExams[cachedIndex].addExam(exam);
+			return;
+		}
+		
 		for (int i = 0; i < termExams.length; i++) {
 			if (termExams[i].wrapsTerm(term)) {
 				termExams[i].addExam(exam);
+				cachedIndex = i;
 			}
 		}
 	}
 	
 	public boolean removeTermExam(TermBean term, ExamBean exam) {
+		if (termExams[cachedIndex].wrapsTerm(term)) {
+			return termExams[cachedIndex].removeExam(exam);
+		}
+		
 		for (int i = 0; i < termExams.length; i++) {
 			if (termExams[i].wrapsTerm(term)) {
+				cachedIndex = i;
 				return termExams[i].removeExam(exam);
 			}
 		}
@@ -37,8 +50,13 @@ public class TermExamMatrix {
 	}
 	
 	public boolean containsTermExam(TermBean term, ExamBean exam) {
+		if (termExams[cachedIndex].wrapsTerm(term)) {
+			return termExams[cachedIndex].containsExam(exam);
+		}
+		
 		for (int i = 0; i < termExams.length; i++) {
 			if (termExams[i].wrapsTerm(term)) {
+				cachedIndex = i;
 				return termExams[i].containsExam(exam);
 			}
 		}
@@ -46,24 +64,35 @@ public class TermExamMatrix {
 		return false;
 	}
 	
+	public ExamBean[] getAllExams(TermBean term) {
+		if (termExams[cachedIndex].wrapsTerm(term)) {
+			return termExams[cachedIndex].getAllExams();
+		}
+		
+		for (int i = 0; i < termExams.length; i++) {
+			if (termExams[i].wrapsTerm(term)) {
+				cachedIndex = i;
+				return termExams[i].getAllExams();
+			}
+		}
+		
+		return null;
+	}
+	
 	// TODO: Možda ovdje odmah računati razmake u danima??? Moglo bi biti dobro...
 	private class TermExams {
-		// TODO: Razmisli treba li ovaj term tu...
-		//private TermBean term;
 		private ExamBean[] exams;
 		private String termID;
 		private int termIDhash;
 		
 		/** Koje mjesto u redu je slobodno (radi bržeg dodavanja) */
-		private int emptySlot = 0;
-		private int slotsNum;
+		private int emptySlot;
 		
 		public TermExams(TermBean term, int examsNum) {
 			this.exams = new ExamBean[examsNum];
-			this.slotsNum = examsNum;
-			//this.term = term;
 			this.termID = term.getTermID();
 			this.termIDhash = term.getTermID().hashCode();
+			this.emptySlot = 0;
 		}
 		
 		public boolean wrapsTerm(TermBean term) {
@@ -72,19 +101,31 @@ public class TermExamMatrix {
 			return (term.getTermID().hashCode() == termIDhash) && term.getTermID().equals(termID);
 		}
 		
+		public ExamBean[] getAllExams() {
+			return exams;
+		}
+		
 		public void addExam(ExamBean exam) {
 			exams[emptySlot] = exam;
-			int start = emptySlot;
-			do {
-				emptySlot = (emptySlot+1) % slotsNum;
-				if (exams[emptySlot] == null) break;
-			} while (emptySlot != start);
+			emptySlot = emptySlot+1;
 		}
 		
 		public boolean removeExam(ExamBean exam) {
 			for (int i = 0; i < exams.length; i++) {
-				if (exams[i] != null && exams[i].getExamID() == exam.getExamID()) {
+				if (exams[i] == null) return false;
+				if (exams[i].getExamID() == exam.getExamID()) {
 					exams[i] = null;
+					while (i+1 < exams.length && exams[i+1] != null) {
+						exams[i] = exams[i+1];
+						i++;
+					}
+					
+					if (i+1 != exams.length) {
+						exams[i+1] = null;
+					}
+					
+					emptySlot--;
+					
 					return true;
 				}
 			}
@@ -94,7 +135,8 @@ public class TermExamMatrix {
 		
 		public boolean containsExam(ExamBean exam) {
 			for (int i = 0; i < exams.length; i++) {
-				if (exams[i] != null && exams[i].getExamID() == exam.getExamID()) {
+				if (exams[i] == null) return false;
+				if (exams[i].getExamID() == exam.getExamID()) {
 					return true;
 				}
 			}
